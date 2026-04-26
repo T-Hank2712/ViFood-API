a30cfd560a10abc7afb9c4aa6668e953f44fa436
LtThanh <157905229+ltthanh2712@users.noreply.github.com>
2026-04-26 15:05:16 +0700

    refactor(ingredient,additive): restructure schema and integrate relat… (#16)
    * refactor(ingredient,additive): restructure schema and integrate related entities (HealthEffect, FoodCategory)
    
    * style: fix flake8 formatting issues

--- app/models/nutrient.py
+++ app/models/nutrient.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import Optional
 
 from pydantic import BaseModel
 
@@ -11,5 +11,5 @@ class Nutrient(BaseModel):
     name: str
     description: Optional[str] = None
     image: Optional[str] = None
-    effects: List[HealthEffect] = []
-    found_in: List[FoodCategory] = []
+    effects: list[HealthEffect] = []
+    found_in: list[FoodCategory] = []
