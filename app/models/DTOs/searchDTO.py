81e09dc92f040276337f94831ed640ebe710cf3c
Lâm Tấn Thành <lamtanthanh2005@gmail.com>
2026-06-15 18:37:12 +0700

    feat(v1/search): implement nutrition search functionality (#47)
    * feat(additive-v1): add "additive management" and relationships with effects and categories
    
    * fix(style): fix formatting issues
    
    * feat(nutrient, ingredient, additive): implement detail queries and graph relationships
    
    * fix(style): fix formatting issues
    
    * feat(search-v1): implement nutrition search functionality
    
    * fix(style): fix formatting issues

--- app/models/DTOs/searchDTO.py
+++ app/models/DTOs/searchDTO.py
@@ -1,15 +1,10 @@
 from pydantic import BaseModel
 
-from app.models.food_category import FoodCategory
-from app.models.health_effect import HealthEffect
-
 
 class searchDTO(BaseModel):
     id: str
     name: str
+    key: str
     code: str | None = None
     description: str | None = None
-    image: str | None = None
-    effects: list[HealthEffect] = []
-    found_in: list[FoodCategory] = []
     type: str
